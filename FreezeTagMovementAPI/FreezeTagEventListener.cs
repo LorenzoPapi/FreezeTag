237d74f53f004f2aa3aa87d05235d01570dd5468
LorenzoPapi <42911751+LorenzoPapi@users.noreply.github.com>
2020-12-13 23:02:10 +0100

    Added chat commands
    Signed-off-by: LorenzoPapi <42911751+LorenzoPapi@users.noreply.github.com>

--- FreezeTagMovementAPI/FreezeTagEventListener.cs
+++ FreezeTagMovementAPI/FreezeTagEventListener.cs
@@ -4,6 +4,7 @@ using Impostor.Api.Games;
 using Impostor.Api.Innersloth;
 using Impostor.Api.Innersloth.Customization;
 using Impostor.Api.Net;
+using Impostor.Api.Net.Inner.Objects;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Concurrent;
@@ -17,6 +18,7 @@ namespace FreezeTag
 {
     public class FreezeTagEventListener : IEventListener
     {
+        private readonly List<IGame> DeactivatedGames = new List<IGame>;
         private readonly Dictionary<IGame, FreezeTagInfos> CodeAndInfos = new Dictionary<IGame, FreezeTagInfos>();
         private readonly ILogger<FreezeTagPlugin> _logger;
 
@@ -28,31 +30,37 @@ namespace FreezeTag
         [EventListener]
         public async void OnGameStarting(IGameStartingEvent e)
         {
-            e.Game.Options.KillCooldown = int.MaxValue;
-            e.Game.Options.NumEmergencyMeetings = 0;
-            await e.Game.SyncSettingsAsync();
+            if (!DeactivatedGames.Contains(e.Game))
+            {
+                e.Game.Options.KillCooldown = int.MaxValue;
+                e.Game.Options.NumEmergencyMeetings = 0;
+                await e.Game.SyncSettingsAsync();
+            }
 
         }
 
         [EventListener]
         public async void OnGameStarted(IGameStartedEvent e)
         {
-            List<IClientPlayer> impostors = new List<IClientPlayer>();
-            ConcurrentDictionary<IClientPlayer, Vector2> frozen = new ConcurrentDictionary<IClientPlayer, Vector2>();
-
-            foreach (var player in e.Game.Players)
+            if (!DeactivatedGames.Contains(e.Game))
             {
-                if (player.Character.PlayerInfo.IsImpostor)
+                List<IClientPlayer> impostors = new List<IClientPlayer>();
+                ConcurrentDictionary<IClientPlayer, Vector2> frozen = new ConcurrentDictionary<IClientPlayer, Vector2>();
+
+                foreach (var player in e.Game.Players)
                 {
-                    await player.Character.SetColorAsync(ColorType.Red);
-                    impostors.Add(player);
+                    if (player.Character.PlayerInfo.IsImpostor)
+                    {
+                        await player.Character.SetColorAsync(ColorType.Red);
+                        impostors.Add(player);
+                    }
+                    else
+                    {
+                        await player.Character.SetColorAsync(ColorType.Green);
+                    }
                 }
-                else
-                {
-                    await player.Character.SetColorAsync(ColorType.Green);
-                }
+                CodeAndInfos.Add(e.Game, new FreezeTagInfos(impostors, frozen));
             }
-            CodeAndInfos.Add(e.Game, new FreezeTagInfos(impostors, frozen));
         }
 
         [EventListener]
@@ -131,18 +139,93 @@ namespace FreezeTag
         [EventListener]
         public void OnGameEnded(IGameEndedEvent e)
         {
-            CodeAndInfos[e.Game].frozens.Clear();
-            CodeAndInfos[e.Game].impostors.Clear();
-            CodeAndInfos.Remove(e.Game);
+            if (CodeAndInfos.ContainsKey(e.Game))
+            {
+                CodeAndInfos[e.Game].frozens.Clear();
+                CodeAndInfos[e.Game].impostors.Clear();
+                CodeAndInfos.Remove(e.Game);
+            }
         }
 
         [EventListener]
         public async void OnPlayerDeath(IPlayerMurderEvent e)
         {
-            foreach (var impostor in CodeAndInfos[e.Game].impostors)
-            {   
-                await impostor.KickAsync();
+            if (CodeAndInfos.ContainsKey(e.Game))
+            {
+                foreach (var impostor in CodeAndInfos[e.Game].impostors)
+                {
+                    await impostor.KickAsync();
+                }
             }
+        }
+
+        [EventListener]
+        public async void OnPlayerChat(IPlayerChatEvent e)
+        {
+            if (e.Game.GameState == GameStates.NotStarted && e.Message.StartsWith("/ftag "))
+            {
+                switch (e.Message.ToLowerInvariant()[6..])
+                {
+                    case "on":
+                        if (e.ClientPlayer.IsHost)
+                        {
+                            if (DeactivatedGames.Contains(e.Game))
+                            {
+                                DeactivatedGames.Remove(e.Game);
+                                await ServerSendChatAsync("Freeze Tag activated for this game.", e.ClientPlayer.Character);
+                            } else
+                            {
+                                await ServerSendChatAsync("Freeze Tag was already active.", e.ClientPlayer.Character);
+                            }
+                        } else
+                        {
+                            await ServerSendChatToPlayerAsync("You can't enable Freeze Tag because you aren't the host.", e.ClientPlayer.Character);
+                        }
+                        break;
+                    case "off":
+                        if (e.ClientPlayer.IsHost)
+                        {
+                            if (!DeactivatedGames.Contains(e.Game))
+                            {
+                                DeactivatedGames.Add(e.Game);
+                                await ServerSendChatAsync("Freeze Tag deactivated for this game.", e.ClientPlayer.Character);
+                            }
+                            else
+                            {
+                                await ServerSendChatAsync("Freeze Tag was already off.", e.ClientPlayer.Character);
+                            }
+                        }
+                        else
+                        {
+                            await ServerSendChatToPlayerAsync("You can't disable Freeze Tag because you aren't the host.", e.ClientPlayer.Character);
+                        }
+                        break;
+                    case "help":
+                        await ServerSendChatToPlayerAsync("Freeze Tag is a custom Among Us mode.", e.ClientPlayer.Character);
+                        await ServerSendChatToPlayerAsync("Impostors are red, crewmates are green. The impostors can freeze the crewmates by standing near them.", e.ClientPlayer.Character);
+                        await ServerSendChatToPlayerAsync("The crewmates can unfreeze the frozen crewmates by standing near of them.", e.ClientPlayer.Character);
+                        await ServerSendChatToPlayerAsync("Objective of the impostors: freeze everyone.", e.ClientPlayer.Character);
+                        await ServerSendChatToPlayerAsync("Objective of the crewmates: finish all their tasks.", e.ClientPlayer.Character);
+                        await ServerSendChatToPlayerAsync("If a crewmate gets killed, the impostors get killed!", e.ClientPlayer.Character);
+                        break;
+                }
+            }
+        }
+
+        private async Task ServerSendChatAsync(string text, IInnerPlayerControl player)
+        {
+            string playername = player.PlayerInfo.PlayerName;
+            await player.SetNameAsync($"PublicMsg");
+            await player.SendChatAsync($"{text}");
+            await player.SetNameAsync(playername);
+        }
+
+        private async Task ServerSendChatToPlayerAsync(string text, IInnerPlayerControl player)
+        {
+            string playername = player.PlayerInfo.PlayerName;
+            await player.SetNameAsync($"PrivateMsg");
+            await player.SendChatToPlayerAsync($"{text}");
+            await player.SetNameAsync(playername);
         }
     }
 }
